f73d9fe0f5f654fc4eab751d4aec987d7cf1d027
Steve McCoole <steve.mccoole@objectpartners.com>
2019-04-29 14:14:52 -0500

    WIP Adding inline documentation.

--- EosioReferenceWalletSignatureProvider/EosioReferenceWalletSignatureProvider.swift
+++ EosioReferenceWalletSignatureProvider/EosioReferenceWalletSignatureProvider.swift
@@ -5,29 +5,39 @@
 //  Copyright (c) 2018-2019 block.one
 //
 
-//  requirements for Devs:
-//
-//  add manifest.json file
-//  add url scheme or universal link
-//  add url scheme to whitelist
-//
-
 import Foundation
 import UIKit
 import EosioSwift
 
+/// Signature provider for EOSIO SDK for Swift that provides selective disclosure and transaction signing using the
+/// EOSIO Reference Wallet Implementation.  Conforms to `EosioSignatureProviderProtocol`.
+///
+/// Requirements for developers.
+/// + Add manifest.json file.
+/// + Add URL scheme or Universal Link.
+/// + Add URL scheme to whitelist.
 public class EosioReferenceWalletSignatureProvider: EosioSignatureProviderProtocol {
 
+    /// Singleton instance of the `EosioReferenceWalletSignatureProvider`.
     public static let shared = EosioReferenceWalletSignatureProvider()
 
+    /// Map of the current completion closures for transaction signatures, keyed by the incoming payload Id.
     static var transactionSignatureCompletions = [String: ((EosioTransactionSignatureResponse) -> Void)]()
+
+    /// Map of the current completion closures for selective disclosure requests, keyed by the incoming payload Id.
     static var selectiveDisclosureCompletions = [String: ((SelectiveDisclosureResponse) -> Void)]()
 
+    /// Handle an incoming request from a Universal Link.
+    ///
+    /// - Parameter userActivity: current application state as `NSUserActivity`.
     public static func handleIncoming(userActivity: NSUserActivity) {
         guard let url = userActivity.webpageURL else { return }
         handleIncoming(url: url)
     }
 
+    /// Handle an incoming request from a custom URL scheme.
+    ///
+    /// - Parameter url: The URL passed in from the system handler that the app has registered.
     public static func handleIncoming(url: URL) {
         guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return }
         guard let queryItems = urlComponents.queryItems else { return }
@@ -38,36 +48,62 @@ public class EosioReferenceWalletSignatureProvider: EosioSignatureProviderProtoc
         handleIncomingTransactionSignature(payload: payload)
     }
 
+    /// If true the incoming request requires biometric authentication to be processed.
     public var requireBiometric = false
+
+    /// The return URL of the request.
     public var returnUrl = ""
+
+    /// The call back URL of the request.
     public var callbackUrl: String?
+
+    /// The declared domain of the request.
     public var declaredDomain: String?
+
+    /// The application manifest of the request.
     public var manifest: String?
+
+    /// Security checks or rules to relax during processing of the current request.
 	public var securityExclusions: SecurityExclusions?
 
+    /// The incoming request structure to handle.
     public struct Request: Codable {
+        /// If set, the incoming request is a transaction signature request.
         public var transactionSignature: EosioTransactionSignatureRequest?
+        /// If set, the incoming request is a selective disclosure request.
         public var selectiveDisclosure: SelectiveDisclosureRequest?
     }
 
-    // Request Payload native struct
-    // Reference: https://github.com/EOSIO/eosio-auth-transport-protocol-specification
+    /// Request Payload native struct
+    /// - seealso: https://github.com/EOSIO/eosio-auth-transport-protocol-specification
     public struct RequestPayload: Codable {
+        /// The payload id.  Should be instance unique.
         public var id = UUID().uuidString
+        /// The payload declared domain for domain matching checks.
         public var declaredDomain: String?
+        /// The paylod return URL.
         public var returnUrl = ""
+        /// The payload call back URL.
         public var callbackUrl: String?
+        /// The payload response key.
         public var responseKey: String?
+        /// If true, the request requires biometric authentication.
         public var requireBiometric: Bool?
+        /// The actual request to be processed.
         public var request = Request()
+        /// Security checks or rules to relax during the processing of this request.
         public var securityExclusions: SecurityExclusions?
 
+        /// The current request in Hex format.
         public var toHex: String? {
             return try? toJsonData().hex
         }
 
         public init() { }
 
+        /// Initialize a request payload from a hexidecimal `String` representation.
+        ///
+        /// - Parameter hex: Hexidecimal `String` representation of a `RequestPayload`.
         public init(hex: String) throws {
             guard let data = Data(hexString: hex) else {
                 throw EosioError(.signatureProviderError, reason: "Invalid request hex string")
@@ -78,24 +114,34 @@ public class EosioReferenceWalletSignatureProvider: EosioSignatureProviderProtoc
         }
     }
 
+    /// Returned response structure for processed request.
     public struct Response: Codable {
+        /// If set, the returned response is a transaction signature response.
         public var transactionSignature: EosioTransactionSignatureResponse?
+        /// if set, the returned response is a selective disclosure response.
         public var selectiveDisclosure: SelectiveDisclosureResponse?
     }
 
-    // Response Payload native struct
-    // Reference: https://github.com/EOSIO/eosio-auth-transport-protocol-specification
+    /// Response Payload native struct
+    /// - seealso: https://github.com/EOSIO/eosio-auth-transport-protocol-specification
     public struct ResponsePayload: Codable {
+        /// The response payload id.  Copied from the incoming request.
         public var id = ""
+        /// The device id that processed the request.
         public var deviceId = ""
+        /// The actual response to be returned.
         public var response = Response()
 
+        /// The current response, in Hex format.
         public var toHex: String? {
             return try? toJsonData().hex
         }
 
         public init() { }
 
+        /// Initialize a response payload from a hexidecimal `String` representation.
+        ///
+        /// - Parameter hex: Hexidecimal `String` representation of a `ResponsePayload`.
         public init(hex: String) throws {
             guard let data = Data(hexString: hex) else {
                 throw EosioError(.signatureProviderError, reason: "Invalid response hex string")
--- EosioReferenceWalletSignatureProvider/SecurityExclusions.swift
+++ EosioReferenceWalletSignatureProvider/SecurityExclusions.swift
@@ -8,24 +8,26 @@
 
 import Foundation
 
+/// Struct to define what security exclusions that the developer is willing to relax for testing.
+/// - Note: Exclusions relax the built in security of the system and are not recommended for production use.
 public struct SecurityExclusions: Codable {
 
-    //If true, the authenticator may skip adding assert actions to the transaction
+    /// If true, the authenticator may skip adding assert actions to the transaction.
     public var addAssertToTransactions = false
 
-    //If true, the authenticator may skip chain manifest and app metadata integrity checks
+    /// If true, the authenticator may skip chain manifest and app metadata integrity checks.
     public var appMetadataIntegrity = false
 
-    //If true, the authenticator may not enforce the same-origin policy
+    /// If true, the authenticator may not enforce the same-origin policy.
     public var domainMatch = false
 
-    //If true, the authenticator may allow signing of non-manifest-whitelisted actions
+    /// If true, the authenticator may allow signing of non-manifest-whitelisted actions.
     public var whitelistedActions = false
 
-    //If true, the authenticator may skip app-, action-, and/or chain-icon integrity checks
+    /// If true, the authenticator may skip app-, action-, and/or chain-icon integrity checks.
     public var iconIntegrity = false
 
-    //If true, the authenticator may allow for parsing of non-compliant Ricardian contracts
+    /// If true, the authenticator may allow for parsing of non-compliant Ricardian contracts.
     public var relaxedContractParsing = false
 
     public init() { }
